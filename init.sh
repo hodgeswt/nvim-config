6ff8e1860c587ca902c0aacd72e0023ff741af50
Will Hodges <hodges.wt@gmail.com>
2024-09-03 18:20:10 -0400

    update init.sh

--- init.sh
+++ init.sh
@@ -1,1 +1,6 @@
 git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+sudo chown -R $(whoami) ~/.local/state/nvim
+if [ ! -d ~/.local/share/nvim/swap ]; then
+    mkdir ~/.local/share/nvim/swap 
+fi
+sudo chown -R $(whoami) ~/.local/share/nvim/swap
