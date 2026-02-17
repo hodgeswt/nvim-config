f390c03d119c225e4caa81151ddb2d607f22c902
Hodges, Will <whodges9@gatech.edu>
2026-02-16 21:13:54 -0500

    keybinding for error popup

--- lua/hodgeswt/remap.lua
+++ lua/hodgeswt/remap.lua
@@ -9,7 +9,8 @@ vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
 vim.keymap.set('n', '<leader>sd', '<cmd>Telescope lsp_document_symbols<CR>', { noremap = true, silent = true })
 vim.keymap.set('n', '<leader>sr', '<cmd>Telescope lsp_references<CR>', { noremap = true, silent = true })
 
-vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, { noremap = true, silent = true})
+vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, { noremap = true, silent = true })
 
-vim.keymap.set('n', '<leader>b', '<C-o>', { noremap = true, silent= true})
+vim.keymap.set('n', '<leader>b', '<C-o>', { noremap = true, silent = true })
 
+vim.keymap.set('n', '<leader>er', vim.diagnostic.open_float, { noremap = true, silent = true })
